b749bcd715bb6ea70a0d75f4a4581ea17a6ec898
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-01-27 01:02:24 +0000

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,24 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
+//  * 
+//     {
+//         includeCover: true, // if includeCover is true the algorithm should show the cover image
+//         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+//         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+//         socialMediaPosition: "right", // social media bar position (left or right)
         
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+//         twitter: null, // social media usernames
+//         github: null,
+//         linkedin: null,
+//         instagram: null,
 
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
+//         name: null,
+//         lastName: null,
+//         role: null,
+//         country: null,
+//         city: null
+//     }
  */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${null ? "Luis" : variables.name}${variables.lastName}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
